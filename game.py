849a04b6af5bd5a095b896f5e985f7d4c61dd95a
Misha Larionov <mishalarionov2@gmail.com>
2016-01-08 16:08:56 -0500

    Some updates

--- game.py
+++ game.py
@@ -1,13 +1,32 @@
-import time
-import os
+import time, random, os, sys
+
+def importantText(n):
+    return("Enter terminal password now (" + str(n) + "/3 attempts left):")
+
+def repetitiveOutput():
+    output("Extracted possible passwords:\n" + wordchoicetext + "\n" + importantText(attempts))
+
 def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def output(str):
-    for i in range(0, len(str)-1):
-        print(str[:i])
-        time.sleep(0.01)
+def ltos(l):
+    s = ""
+    for i in range(0, len(l)):
+        s = (s + l[i] + " ")
+    s = s.strip()
+    return s
+
+def repetitivelist(n, i):
+    l = []
+    for z in range(0, n):
+        l.append(random.choice(i))
+    return l
+
+def output(string):
+    for i in range(0, len(string)+1):
         cls()
+        print(string[:i])
+        time.sleep(0.01)
 
 def readfile(n):
         l = []
@@ -16,9 +35,32 @@ def readfile(n):
                 line = line.strip()
                 if len(line) == n:
                     l.append(line)
-        print(l)
         f.close()
+        return l
 
 input("Press ENTER to run. Do not run from IDLE.")
 
-readfile(5)
+attempts = 3
+wordlist = readfile(5)
+passGuessed = False
+
+wordchoice = repetitivelist(8, wordlist)
+wordchoicetext = ltos(wordchoice).upper()
+password = random.choice(wordchoice).strip()
+repetitiveOutput()
+
+while attempts > 0 and passGuessed == False:
+    userinput = input()
+    userinput = userinput.strip().upper()
+    if userinput == password:
+        passGuessed = True
+    else:
+        attempts = attempts - 1
+        
+print("ayy lmao")
+input()
+sys.exit()
+
+
+
+
